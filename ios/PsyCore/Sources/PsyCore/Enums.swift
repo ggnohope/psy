94a62132c9bcb9a7bab06bebe2456cb5c5aec935
hoalam <lamhoa9a1thcsttlp@gmail.com>
2026-06-20 10:13:08 +0700

    feat(ios): domain enums and value-type models in PsyCore

--- ios/PsyCore/Sources/PsyCore/Enums.swift
+++ ios/PsyCore/Sources/PsyCore/Enums.swift
@@ -4,3 +4,15 @@ public enum TxType: String, Codable, Sendable, CaseIterable {
     case expense = "EXPENSE"
     case transfer = "TRANSFER"
 }
+
+public enum AccountType: String, Codable, Sendable, CaseIterable {
+    case cash = "CASH"
+    case bank = "BANK"
+    case credit = "CREDIT"
+    case asset = "ASSET"
+}
+
+public enum CategoryType: String, Codable, Sendable, CaseIterable {
+    case income = "INCOME"
+    case expense = "EXPENSE"
+}
